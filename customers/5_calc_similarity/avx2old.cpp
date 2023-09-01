4777577393de57dc614d4f52adfbd4ff132ccdb8
archibate <1931127624@qq.com>
2023-09-01 13:44:09 +0800

    fin

--- customers/5_calc_similarity/avx2old.cpp
+++ customers/5_calc_similarity/avx2old.cpp
@@ -1,9 +1,10 @@
 //********************************************************************************************
 //***********************************normal_calSimilarity.cpp********************************
 //********************************************************************************************
-#define USE_MULTISCORE
+//#define USE_MULTISCORE//causing wrong result
 //#define USE_FIRSTUNROLL
-//#define USE_OPENMP
+#define USE_OPENMP
+#define USE_UNROLLM4
 #include <iostream>
 #include <vector>
 #include <random>
@@ -76,7 +77,7 @@ static float multiScore(templateFeatSimd const *template_point_simd,
                         float PartialSumBase) {
     auto $PartialScore = _mm256_setzero_ps();
     auto $PartialSum = _mm256_set1_ps(PartialSumBase);
-    auto $SumOfCoords = _mm256_add_ps(_mm256_setr_ps(-7, -6, -5, -4, -3, -2, -1, 0), _mm256_set1_ps(mbase));
+    auto $SumOfCoords = _mm256_add_ps(_mm256_setr_ps(1, 2, 3, 4, 5, 6, 7, 8), _mm256_set1_ps(mbase));
     auto $8 = _mm256_set1_ps(8);
     auto $template_feat_size_plus1 = _mm256_set1_ps(template_feat_size + 1);
     auto $i = _mm256_set1_epi32(i);
@@ -85,10 +86,9 @@ static float multiScore(templateFeatSimd const *template_point_simd,
     auto $230 = _mm256_set1_epi32(230);
     auto $350 = _mm256_set1_epi32(350);
 
-    for(int m = mbase; m < ((template_feat_size + 7) & ~7); m += 8)
-        {
-        $SumOfCoords = _mm256_add_ps($SumOfCoords, $8);
-
+    /* printf("%d\n", mbase); */
+    for(int m = mbase; m < ((template_feat_size + 7) & ~7); m += 8, $SumOfCoords = _mm256_add_ps($SumOfCoords, $8))
+    {
         auto $curX = _mm256_add_epi32($i, template_point_simd[m / 8].x);
         auto $curY = _mm256_add_epi32($j, template_point_simd[m / 8].y);
 
@@ -125,12 +125,20 @@ static float multiScore(templateFeatSimd const *template_point_simd,
         /*      printf("%d %f %f %f %f %f %f %f %f %f %f %f\n", m + _, $accum[_], _mm256_cvtepi32_ps($offSet)[_], _mm256_cvtepi32_ps($curX)[_], _mm256_cvtepi32_ps($curY)[_], _mm256_cvtepi32_ps($iSx)[_], _mm256_cvtepi32_ps($iSy)[_], _mm256_cvtepi32_ps($iTx)[_], _mm256_cvtepi32_ps($iTy)[_], $iSm[_], $iTm[_], _mm256_cvtepi32_ps($notok)[_]); */
         /* exit(1); */
 
-        // 0 1 2 3 (HI)
+        /* for (int _ = 0; _ < 8; _++) */
+        /*      printf("%d %f\n", _, $accum[_]); */
+
+        // 0 1 2 3 4 5 6 7
         $accum = _mm256_add_ps($accum, _mm256_castsi256_ps(_mm256_slli_si256(_mm256_castps_si256($accum), 4)));
-        // 0 1+0 2+1 3+2
+        // 0 1+0 2+1 3+2 4 5+4 6+5 7+6
         $accum = _mm256_add_ps($accum, _mm256_castsi256_ps(_mm256_slli_si256(_mm256_castps_si256($accum), 8)));
-        // 0 1+0 2+1+0 3+2+1+0
-        $accum = _mm256_add_ps($accum, _mm256_set_m128(_mm256_castps256_ps128($accum), _mm_setzero_ps()));
+        // 0 1+0 2+1+0 3+2+1+0 4 5+4 6+5+4 7+6+5+4
+        $accum = _mm256_add_ps($accum, _mm256_set_m128(_mm_permute_ps(_mm256_castps256_ps128($accum), 0xff), _mm_setzero_ps()));
+        // 0 1+0 2+1+0 3+2+1+0 3+2+1+0+4 3+2+1+0+5+4 3+2+1+0+6+5+4 3+2+1+0+7+6+5+4
+
+        /* for (int _ = 0; _ < 8; _++) */
+        /*      printf("%d %f\n", _, $accum[_]); */
+        /* exit(1); */
 
         $PartialScore = _mm256_add_ps($PartialSum, $accum);
         /* auto $cond = _mm256_mul_ps($SumOfCoords, _mm256_min_ps( */
@@ -139,12 +147,17 @@ static float multiScore(templateFeatSimd const *template_point_simd,
         auto $cond = _mm256_load_ps(thresholds + m);
         auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmp_ps($PartialScore, $cond, _CMP_LT_OQ));
         int cmpmask = _mm256_movemask_ps($cmp);
-        if (cmpmask) {
-        int bit = __builtin_ctz(cmpmask);
-        return $PartialScore[bit] / (m + bit + 1);
+        if (cmpmask) [[unlikely]] {
+            int bit = __builtin_ctz(cmpmask);
+            return $PartialScore[bit] / (m + bit + 1);
         }
-        $PartialSum = _mm256_add_ps($PartialSum, _mm256_shuffle_ps($accum, $accum, 0xff));
-        }
+        $PartialSum = _mm256_add_ps($PartialSum, _mm256_broadcastss_ps(_mm256_castps256_ps128(_mm256_castpd_ps(_mm256_permute4x64_pd(_mm256_castps_pd(_mm256_permute_ps($accum, 0xff)), 0xf)))));
+    }
+
+        /* for (int _ = 0; _ < 8; _++) */
+        /*      printf("%d %f %f\n", _, $PartialSum[_], $SumOfCoords[_]); */
+        /* exit(1); */
+
     $PartialScore = _mm256_div_ps($PartialScore, $SumOfCoords);
     return $PartialScore[(template_feat_size - 1) & 7];
 }
@@ -159,11 +172,11 @@ std::vector<matchResult> const &calSimilarity(const std::vector<templateFeat> &t
     static std::vector<templateFeatSimd> template_point_simd;
     template_point_simd.resize((template_feat_size + 7) & ~7);
     for (int m = 0; m < template_point_simd.size() * 8; m += 8) {
-        template_point_simd[m / 4].x = _mm256_setr_epi32(template_point[m].x, template_point[m + 1].x, template_point[m + 2].x, template_point[m + 3].x, template_point[m + 4].x, template_point[m + 4 + 1].x, template_point[m + 4 + 2].x, template_point[m + 4 + 3].x);
-        template_point_simd[m / 4].y = _mm256_setr_epi32(template_point[m].y, template_point[m + 1].y, template_point[m + 2].y, template_point[m + 3].y, template_point[m + 4].y, template_point[m + 4 + 1].y, template_point[m + 4 + 2].y, template_point[m + 4 + 3].y);
-        template_point_simd[m / 4].dx = _mm_setr_epi16(template_point[m].dx, template_point[m + 1].dx, template_point[m + 2].dx, template_point[m + 3].dx, template_point[m + 4].dx, template_point[m + 4 + 1].dx, template_point[m + 4 + 2].dx, template_point[m + 4 + 3].dx);
-        template_point_simd[m / 4].dy = _mm_setr_epi16(template_point[m].dy, template_point[m + 1].dy, template_point[m + 2].dy, template_point[m + 3].dy, template_point[m + 4].dy, template_point[m + 4 + 1].dy, template_point[m + 4 + 2].dy, template_point[m + 4 + 3].dy);
-        template_point_simd[m / 4].mag = _mm256_setr_ps(template_point[m].mag, template_point[m + 1].mag, template_point[m + 2].mag, template_point[m + 3].mag, template_point[m + 4].mag, template_point[m + 4 + 1].mag, template_point[m + 4 + 2].mag, template_point[m + 4 + 3].mag);
+        template_point_simd[m / 8].x = _mm256_setr_epi32(template_point[(m) % template_feat_size].x, template_point[(m + 1) % template_feat_size].x, template_point[(m + 2) % template_feat_size].x, template_point[(m + 3) % template_feat_size].x, template_point[(m + 4) % template_feat_size].x, template_point[(m + 4 + 1) % template_feat_size].x, template_point[(m + 4 + 2) % template_feat_size].x, template_point[(m + 4 + 3) % template_feat_size].x);
+        template_point_simd[m / 8].y = _mm256_setr_epi32(template_point[(m) % template_feat_size].y, template_point[(m + 1) % template_feat_size].y, template_point[(m + 2) % template_feat_size].y, template_point[(m + 3) % template_feat_size].y, template_point[(m + 4) % template_feat_size].y, template_point[(m + 4 + 1) % template_feat_size].y, template_point[(m + 4 + 2) % template_feat_size].y, template_point[(m + 4 + 3) % template_feat_size].y);
+        template_point_simd[m / 8].dx = _mm_setr_epi16(template_point[(m) % template_feat_size].dx, template_point[(m + 1) % template_feat_size].dx, template_point[(m + 2) % template_feat_size].dx, template_point[(m + 3) % template_feat_size].dx, template_point[(m + 4) % template_feat_size].dx, template_point[(m + 4 + 1) % template_feat_size].dx, template_point[(m + 4 + 2) % template_feat_size].dx, template_point[(m + 4 + 3) % template_feat_size].dx);
+        template_point_simd[m / 8].dy = _mm_setr_epi16(template_point[(m) % template_feat_size].dy, template_point[(m + 1) % template_feat_size].dy, template_point[(m + 2) % template_feat_size].dy, template_point[(m + 3) % template_feat_size].dy, template_point[(m + 4) % template_feat_size].dy, template_point[(m + 4 + 1) % template_feat_size].dy, template_point[(m + 4 + 2) % template_feat_size].dy, template_point[(m + 4 + 3) % template_feat_size].dy);
+        template_point_simd[m / 8].mag = _mm256_setr_ps(template_point[(m) % template_feat_size].mag, template_point[(m + 1) % template_feat_size].mag, template_point[(m + 2) % template_feat_size].mag, template_point[(m + 3) % template_feat_size].mag, template_point[(m + 4) % template_feat_size].mag, template_point[(m + 4 + 1) % template_feat_size].mag, template_point[(m + 4 + 2) % template_feat_size].mag, template_point[(m + 4 + 3) % template_feat_size].mag);
     }
 #endif
 
@@ -192,9 +205,10 @@ std::vector<matchResult> const &calSimilarity(const std::vector<templateFeat> &t
     {
         for(int j = 0; j < 1200; j += 8)
         {
+            int m;
             auto $PartialSum   = _mm256_setzero_ps();
             #ifdef USE_FIRSTUNROLL
-            auto $SumOfCoords  = _mm256_set1_epi32(2);
+            auto $SumOfCoords  = _mm256_set1_epi32(1);
             #else
             auto $SumOfCoords  = _mm256_set1_epi32(1);
             #endif
@@ -205,9 +219,9 @@ std::vector<matchResult> const &calSimilarity(const std::vector<templateFeat> &t
             #ifdef USE_FIRSTUNROLL
             {
                 int curX = i + template_point[0].x;
-                if (!(curX < 0 || curX > 230)) {
+                if (!(curX < 0 || curX > 230)) [[likely]] {
                     auto $curY = _mm256_add_epi32($j, _mm256_set1_epi32(template_point[0].y));
-                    auto $notok = _mm256_or_si256($loopterm, _mm256_or_si256(_mm256_srai_epi32($curY, 31), _mm256_cmpgt_epi32($curY, $350)));
+                    auto $notok = _mm256_or_si256(_mm256_srai_epi32($curY, 31), _mm256_cmpgt_epi32($curY, $350));
 
                     int iTx = template_point[0].dx;
                     int iTy = template_point[0].dy;
@@ -230,74 +244,113 @@ std::vector<matchResult> const &calSimilarity(const std::vector<templateFeat> &t
                                                 _mm256_mullo_epi32($iSy, _mm256_set1_epi32(iTy))))));
                     $PartialSum = _mm256_add_ps($PartialSum, $accum);
 
-                    auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmplt_ps($PartialSum, _mm256_set1_ps(thresholds[0])));
+                    auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmp_ps($PartialSum, _mm256_set1_ps(thresholds[0]), _CMP_LT_OQ));
+                    $loopterm = _mm256_castps_si256($cmp);
+                    $SumOfCoords = _mm256_add_epi32($SumOfCoords, _mm256_andnot_si256($loopterm, _mm256_set1_epi32(1)));
+                }
+            }
+            if (_mm256_movemask_epi8($loopterm) != -1) {
+                int curX = i + template_point[1].x;
+                if (!(curX < 0 || curX > 230)) [[likely]] {
+                    auto $curY = _mm256_add_epi32($j, _mm256_set1_epi32(template_point[1].y));
+                    auto $notok = _mm256_or_si256($loopterm, _mm256_or_si256(_mm256_srai_epi32($curY, 31), _mm256_cmpgt_epi32($curY, $350)));
+
+                    int iTx = template_point[1].dx;
+                    int iTy = template_point[1].dy;
+                    float iTm = template_point[1].mag;
+
+                    /* int offSet = curY * 350 + curX; */
+                    auto $offSet = _mm256_andnot_si256($notok, _mm256_add_epi32(_mm256_mullo_epi32($curY, $350), _mm256_set1_epi32(curX)));
+                    auto $iSxy = _mm256_i32gather_epi32((int *)search_point.data(), $offSet, sizeof(searchFeat));
+                    // isx isy isx isy isx isy isx isy
+                    auto $iSx = _mm256_srai_epi32(_mm256_slli_epi32($iSxy, 16), 16);
+                    auto $iSy = _mm256_srai_epi32($iSxy, 16);
+                    auto $iSm = _mm256_i32gather_ps(1 + (float *)search_point.data(), $offSet, sizeof(searchFeat));
+
+                    auto $accum = _mm256_andnot_ps(_mm256_castsi256_ps($notok),
+                                                _mm256_mul_ps(
+                                                _mm256_mul_ps($iSm, _mm256_set1_ps(iTm)),
+                                                _mm256_cvtepi32_ps(
+                                                _mm256_add_epi32(
+                                                _mm256_mullo_epi32($iSx, _mm256_set1_epi32(iTx)),
+                                                _mm256_mullo_epi32($iSy, _mm256_set1_epi32(iTy))))));
+                    $PartialSum = _mm256_add_ps($PartialSum, $accum);
+
+                    auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmp_ps($PartialSum, _mm256_set1_ps(thresholds[1]), _CMP_LT_OQ));
                     $loopterm = _mm256_or_si256($loopterm, _mm256_castps_si256($cmp));
+                    $SumOfCoords = _mm256_add_epi32($SumOfCoords, _mm256_andnot_si256($loopterm, _mm256_set1_epi32(1)));
+                }
+                if (_mm256_movemask_epi8($loopterm) != -1) [[unlikely]] {
+                    m = 2;
+                    #else
+                    m = 0;
+                    #endif
+
+                    #ifdef USE_UNROLLM4
+                    #pragma GCC unroll 4
+                    #endif
+                    for(;
+                        m < template_feat_size;
+                        m++, $SumOfCoords = _mm256_add_epi32($SumOfCoords, _mm256_andnot_si256($loopterm, _mm256_set1_epi32(1)))
+                    ) {
+                        int curX = i + template_point[m].x;
+                        if (curX < 0 || curX > 230) [[unlikely]] { continue; }
+
+                        auto $curY = _mm256_add_epi32($j, _mm256_set1_epi32(template_point[m].y));
+                        auto $notok = _mm256_or_si256($loopterm, _mm256_or_si256(_mm256_srai_epi32($curY, 31), _mm256_cmpgt_epi32($curY, $350)));
+
+                        int iTx = template_point[m].dx;
+                        int iTy = template_point[m].dy;
+                        float iTm = template_point[m].mag;
+
+                        /* int offSet = curY * 350 + curX; */
+                        auto $offSet = _mm256_andnot_si256($notok, _mm256_add_epi32(_mm256_mullo_epi32($curY, $350), _mm256_set1_epi32(curX)));
+                        auto $iSxy = _mm256_i32gather_epi32((int *)search_point.data(), $offSet, sizeof(searchFeat));
+                        // isx isy isx isy isx isy isx isy
+                        auto $iSx = _mm256_srai_epi32(_mm256_slli_epi32($iSxy, 16), 16);
+                        auto $iSy = _mm256_srai_epi32($iSxy, 16);
+                        auto $iSm = _mm256_i32gather_ps(1 + (float *)search_point.data(), $offSet, sizeof(searchFeat));
+
+                        auto $accum = _mm256_andnot_ps(_mm256_castsi256_ps($notok),
+                                                    _mm256_mul_ps(
+                                                    _mm256_mul_ps($iSm, _mm256_set1_ps(iTm)),
+                                                    _mm256_cvtepi32_ps(
+                                                    _mm256_add_epi32(
+                                                    _mm256_mullo_epi32($iSx, _mm256_set1_epi32(iTx)),
+                                                    _mm256_mullo_epi32($iSy, _mm256_set1_epi32(iTy))))));
+                        $PartialSum = _mm256_add_ps($PartialSum, $accum);
+
+                        auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmp_ps($PartialSum, _mm256_set1_ps(thresholds[m]), _CMP_LT_OQ));
+                        $loopterm = _mm256_or_si256($loopterm, _mm256_castps_si256($cmp));
+                        #ifndef USE_MULTISCORE
+                        if (_mm256_movemask_epi8($loopterm) == -1) {
+                            break;
+                        }
+                        #else
+                        auto popcorn = __builtin_popcount(_mm256_movemask_ps(_mm256_castsi256_ps($loopterm)));
+                        if ((popcorn == 8 || popcorn >= 7) && !(m & 7)) [[unlikely]] {
+                            break;
+                        }
+                        #endif
+                    }
+            #ifdef USE_FIRSTUNROLL
                 }
             }
-            int m = 1;
-            #else
-            int m = 0;
             #endif
-
-            #pragma GCC unroll 4
-            for(;
-                m < template_feat_size;
-                m++, $SumOfCoords = _mm256_add_epi32($SumOfCoords, _mm256_andnot_si256($loopterm, _mm256_set1_epi32(1)))
-            ) {
-                int curX = i + template_point[m].x;
-                if (curX < 0 || curX > 230) continue;
-
-                auto $curY = _mm256_add_epi32($j, _mm256_set1_epi32(template_point[m].y));
-                auto $notok = _mm256_or_si256($loopterm, _mm256_or_si256(_mm256_srai_epi32($curY, 31), _mm256_cmpgt_epi32($curY, $350)));
-
-                int iTx = template_point[m].dx;
-                int iTy = template_point[m].dy;
-                float iTm = template_point[m].mag;
-
-                /* int offSet = curY * 350 + curX; */
-                auto $offSet = _mm256_andnot_si256($notok, _mm256_add_epi32(_mm256_mullo_epi32($curY, $350), _mm256_set1_epi32(curX)));
-                auto $iSxy = _mm256_i32gather_epi32((int *)search_point.data(), $offSet, sizeof(searchFeat));
-                // isx isy isx isy isx isy isx isy
-                auto $iSx = _mm256_srai_epi32(_mm256_slli_epi32($iSxy, 16), 16);
-                auto $iSy = _mm256_srai_epi32($iSxy, 16);
-                auto $iSm = _mm256_i32gather_ps(1 + (float *)search_point.data(), $offSet, sizeof(searchFeat));
-
-                auto $accum = _mm256_andnot_ps(_mm256_castsi256_ps($notok),
-                                            _mm256_mul_ps(
-                                            _mm256_mul_ps($iSm, _mm256_set1_ps(iTm)),
-                                            _mm256_cvtepi32_ps(
-                                            _mm256_add_epi32(
-                                            _mm256_mullo_epi32($iSx, _mm256_set1_epi32(iTx)),
-                                            _mm256_mullo_epi32($iSy, _mm256_set1_epi32(iTy))))));
-                $PartialSum = _mm256_add_ps($PartialSum, $accum);
-
-                auto $cmp = _mm256_andnot_ps(_mm256_castsi256_ps($notok), _mm256_cmp_ps($PartialSum, _mm256_set1_ps(thresholds[m]), _CMP_LT_OQ));
-                $loopterm = _mm256_or_si256($loopterm, _mm256_castps_si256($cmp));
-                #ifndef USE_MULTISCORE
-                if (_mm256_movemask_epi8($loopterm) == 0xffffffff) {
-                    break;
-                }
-                #else
-                auto popcorn = __builtin_popcount(_mm256_movemask_epi8($loopterm));
-                if (popcorn == 32 || (popcorn >= 28 && !(m & 7))) {
-                    break;
-                }
-                #endif
-            }
 
             auto $PartialScore = _mm256_div_ps($PartialSum, _mm256_cvtepi32_ps($SumOfCoords));
             #ifdef USE_MULTISCORE
-            auto mask = _mm256_movemask_epi8($loopterm);
-            if (mask != -1 && m < template_feat_size) {
-                auto k = __builtin_ctz(~mask) >> 2;
+            auto mask = _mm256_movemask_ps(_mm256_castsi256_ps($loopterm));
+            if (mask != 0xff && m < template_feat_size) {
+                auto k = __builtin_ctz(~mask);
                 $PartialScore[k] = multiScore(
                     template_point_simd.data(), search_point.data(), thresholds.data(),
                     template_feat_size, search_feat_size, i, j, m, $PartialSum[k]);
             }
             #endif
+            auto $PartialScoreAbove = _mm256_cmp_ps($PartialScore, _mm256_set1_ps(0.4f), _CMP_GT_OQ);
             for (int k = 0; k < 8; k++) {
-                if($PartialScore[k] > 0.4f)
-                {
+                if($PartialScoreAbove[k]) {
                 /* printf("%d %d %f\n", i, j, PartialScore); */
                 /*     exit(1); */
 #ifdef USE_OPENMP
