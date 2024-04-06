a30ee38597904c57d65e44f13f49112732d5d34a
archibate <1931127624@qq.com>
2024-04-06 17:33:40 +0800

    corotest

--- foundation/cppcorotest/main.cpp
+++ foundation/cppcorotest/main.cpp
@@ -1,3 +1,4 @@
+#include "debug.hpp"
 #include <chrono>
 #include <coroutine>
 #include <cstdio>
@@ -5,10 +6,11 @@
 #include <functional>
 #include <optional>
 #include <span>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include "debug.hpp"
+#include <compare>
+#include <atomic>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std::literals;
 
@@ -16,19 +18,80 @@ auto repr(std::ostream &os, std::coroutine_handle<> h) {
     os << "coroutine@" << h.address();
 }
 
-struct scheduler {
-    void add(std::coroutine_handle<> h) {
-        std::unique_lock lck(mtx);
-        ready.push_front(h);
-        cv.notify_one();
+struct epoll_file {
+    epoll_file(int fd = -1) : epollfd(fd) {}
+
+    epoll_file(epoll_file &&that) : epollfd(that.epollfd) {
+        that.epollfd = -1;
     }
 
-    void batched_add(std::ranges::input_range auto &&hs) {
-        std::unique_lock lck(mtx);
-        for (auto const &h: hs) {
-            ready.push_front(h);
-        }
-        cv.notify_all();
+    epoll_file &operator=(epoll_file &&that) {
+        if (epollfd >= 0)
+            close(epollfd);
+        epollfd = that.epollfd;
+        that.epollfd = -1;
+        return *this;
+    }
+
+    ~epoll_file() {
+        if (epollfd >= 0)
+            close(epollfd);
+    }
+
+    auto wait(std::span<epoll_event> events, int milliseconds = -1) {
+        return epoll_wait(epollfd, events.data(), events.size(), milliseconds);
+    }
+
+    auto add(int fd, epoll_event event) {
+        return epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    }
+
+    auto mod(int fd, epoll_event event) {
+        return epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+    }
+
+    auto del(int fd, epoll_event event) {
+        return epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &event);
+    }
+
+    int epollfd;
+};
+
+struct heap_timer {
+    explicit heap_timer(std::chrono::system_clock::time_point expire, std::coroutine_handle<> h)
+        : expire(expire)
+        , h(h) {
+    }
+
+    auto operator<(const heap_timer &that) const {
+        return expire > that.expire;
+    }
+
+    std::chrono::system_clock::time_point expire;
+    std::coroutine_handle<> h;
+};
+
+struct time_heap { // smallest timestamp first heap
+    void push(const heap_timer &timer) {
+        heap.push_back(timer);
+        std::push_heap(heap.begin(), heap.end());
+    }
+
+    void pop() {
+        std::pop_heap(heap.begin(), heap.end());
+        heap.pop_back();
+    }
+
+    auto &top() {
+        return heap.front();
+    }
+
+    std::vector<heap_timer> heap;
+};
+
+struct scheduler {
+    void add(std::coroutine_handle<> h) {
+        ready.push_front(h);
     }
 
     void run_until_done(std::coroutine_handle<> h) {
@@ -39,18 +102,52 @@ struct scheduler {
     }
 
     void run() {
-        std::unique_lock lck(mtx);
-        cv.wait(lck, [this] { return !ready.empty(); });
-        auto h = ready.front();
-        ready.pop_front();
-        lck.unlock();
-        h.resume();
+        while (ready.empty()) {
+            int timeout = -1;
+            if (!timers.heap.empty()) {
+                auto &timer = timers.top();
+                auto now = std::chrono::system_clock::now();
+                if (now >= timer.expire) {
+                    add(timer.h);
+                    timers.pop();
+                    break;
+                } else {
+                    timeout = std::chrono::duration_cast<std::chrono::milliseconds>(timer.expire - now).count();
+                }
+            }
+            auto num = ep.wait(events, timeout);
+            if (num < 0 || num > events.size())
+                throw std::system_error(errno, std::system_category());
+            for (std::size_t i = 0; i < num; i++) {
+                auto &e = events[i];
+                auto h = std::coroutine_handle<>::from_address(e.data.ptr);
+                add(h);
+            }
+        }
+        while (!ready.empty()) {
+            auto h = ready.front();
+            ready.pop_front();
+            h.resume();
+        }
+    }
+
+    void async_timer(std::chrono::system_clock::time_point expire, std::coroutine_handle<> last) {
+        timers.push(heap_timer{expire, last});
+    }
+
+    void async_wait_file(int fd, std::uint32_t events, std::coroutine_handle<> last) {
+        epoll_event event;
+        event.events = events;
+        event.data.ptr = last.address();
+        if (ep.add(fd, event) == -1)
+            throw std::system_error(errno, std::system_category());
     }
 
 private:
-    std::mutex mtx;
-    std::condition_variable cv;
     std::deque<std::coroutine_handle<>> ready;
+    epoll_file ep{epoll_create1(0)};
+    time_heap timers;
+    std::vector<epoll_event> events{1024};
 };
 
 scheduler &get_scheduler() {
@@ -262,35 +359,63 @@ private:
     std::coroutine_handle<promise_type> curr;
 };
 
-struct awaiter_duration {
-    awaiter_duration(std::chrono::system_clock::duration d) : duration(d) {}
-
-    bool await_ready() {
-        return duration.count() <= 0;
-    }
-
-    void await_resume() {
-    }
-
-    void await_suspend(std::coroutine_handle<> last) noexcept {
-        std::thread([last, duration = duration] {
-            std::this_thread::sleep_for(duration);
-            get_scheduler().add(last);
-        }).detach();
-    }
-
-private:
-    std::chrono::system_clock::duration duration;
-};
-
 template <class T = void>
 struct task : basic_task<basic_promise<T>> {
     using basic_task<basic_promise<T>>::basic_task;
 };
 
+struct awaiter_time_point {
+    awaiter_time_point(std::chrono::system_clock::time_point expire) : expire(expire) {}
+
+    bool await_ready() {
+        return false;
+    }
+
+    void await_resume() {
+    }
+
+    void await_suspend(std::coroutine_handle<> last) {
+        get_scheduler().async_timer(expire, last);
+    }
+
+private:
+    std::chrono::system_clock::time_point expire;
+};
+
+struct awaiter_wait_file {
+    awaiter_wait_file(int fd, std::uint32_t events) : fd(fd), events(events) {}
+
+    bool await_ready() {
+        return false;
+    }
+
+    void await_resume() {
+    }
+
+    void await_suspend(int fd, std::uint32_t events, std::coroutine_handle<> last) {
+        get_scheduler().async_wait_file(fd, events, last);
+    }
+
+private:
+    int fd;
+    std::uint32_t events;
+};
+
 template <class Rep, class Period>
 task<> sleep_for(std::chrono::duration<Rep, Period> duration) {
-    co_await awaiter_duration(std::move(duration));
+    if (duration.count() > 0) {
+        co_await awaiter_time_point(std::chrono::system_clock::now() + duration);
+    }
+}
+
+template <class Clock, class Dur>
+task<> sleep_until(std::chrono::time_point<Clock, Dur> time_point) {
+    co_await awaiter_time_point(time_point);
+}
+
+template <class T>
+task<T> wait_file(int fd, std::uint32_t events) {
+    co_await awaiter_wait_file(fd, events);
 }
 
 struct awaiter_count {
