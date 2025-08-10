e2cf240976949708dc9e37e525bebb90818d4d9a
archibate <1931127624@qq.com>
2025-08-10 13:13:19 +0800

    v7

---
+++ foundation/lockfreequeue/v7_rw_pos_localize_ring_queue.cpp
@@ -0,0 +1,79 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <thread>
+#include "show_time.h"
+#include <smmintrin.h>
+
+
+alignas(64) int ring[1024];
+alignas(64) std::atomic_int write_pos;
+alignas(64) std::atomic_int read_pos;
+alignas(64) int read_pos_cached;
+alignas(64) int write_pos_cached;
+alignas(64) int write_pos_local;
+alignas(64) int read_pos_local;
+
+void ring_push(int value)
+{
+again:
+    int w = write_pos_local;
+    int next_w = (w + 1) % 1024;
+    if (next_w == read_pos_cached) {
+        read_pos_cached = read_pos.load(std::memory_order_relaxed);
+        if (next_w == read_pos_cached) {
+            goto again;
+        }
+    }
+    ring[w] = value;
+    write_pos_local = next_w;
+    write_pos.store(next_w, std::memory_order_release);
+}
+
+int ring_pop()
+{
+again:
+    int r = read_pos_local;
+    if (r == write_pos_cached) {
+        write_pos_cached = write_pos.load(std::memory_order_acquire);
+        if (r == write_pos_cached) {
+            goto again;
+        }
+    }
+    int next_r = (r + 1) % 1024;
+    int value = ring[r];
+    read_pos_local = next_r;
+    read_pos.store(next_r, std::memory_order_relaxed);
+    return value;
+}
+
+
+const int N = 1024 * 1024;
+
+void producer()
+{
+    for (int i = 0; i < N; ++i) {
+        ring_push(i);
+    }
+}
+
+void consumer()
+{
+    for (int i = 0; i < N; ++i) {
+        int value = ring_pop();
+        if (value != i) [[unlikely]] {
+            fmt::println("Data Error: {} != {}", value, i);
+            exit(1);
+        }
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 100; ++i) {
+        show_time _("queue");
+        std::jthread producer_thread(producer);
+        std::jthread consumer_thread(consumer);
+        producer_thread.join();
+        consumer_thread.join();
+    }
+}
