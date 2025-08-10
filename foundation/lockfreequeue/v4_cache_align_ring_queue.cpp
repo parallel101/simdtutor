1c0ed9a0667a51c3e560ac7e38e438188bf46b36
archibate <1931127624@qq.com>
2025-08-10 13:07:39 +0800

    v43

---
+++ foundation/lockfreequeue/v4_cache_align_ring_queue.cpp
@@ -0,0 +1,67 @@
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <thread>
+#include "show_time.h"
+#include <smmintrin.h>
+
+
+alignas(64) int ring[1024];
+alignas(64) std::atomic_int write_pos;
+alignas(64) std::atomic_int read_pos;
+
+void ring_push(int value)
+{
+again:
+    int w = write_pos.load(std::memory_order_relaxed);
+    int next_w = (w + 1) % 1024;
+    if (next_w == read_pos.load(std::memory_order_relaxed)) {
+        goto again;
+    }
+    ring[w] = value;
+    write_pos.store(next_w, std::memory_order_release);
+}
+
+int ring_pop()
+{
+again:
+    int r = read_pos.load(std::memory_order_relaxed);
+    if (r == write_pos.load(std::memory_order_acquire)) {
+        goto again;
+    }
+    int next_r = (r + 1) % 1024;
+    int value = ring[r];
+    read_pos.store(next_r, std::memory_order_relaxed);
+    return value;
+}
+
+
+const int N = 1024 * 1024;
+
+void producer()
+{
+    for (int i = 0; i < N; ++i) {
+        ring_push(i);
+    }
+}
+
+void consumer()
+{
+    for (int i = 0; i < N; ++i) {
+        int value = ring_pop();
+        if (value != i) [[unlikely]] {
+            fmt::println("Data Error: {} != {}", value, i);
+            exit(1);
+        }
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 100; ++i) {
+        show_time _("queue");
+        std::jthread producer_thread(producer);
+        std::jthread consumer_thread(consumer);
+        producer_thread.join();
+        consumer_thread.join();
+    }
+}
