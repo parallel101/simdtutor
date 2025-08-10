e131560b2139ad374621cce8721cb49e84dfa9bf
archibate <1931127624@qq.com>
2025-08-10 13:03:39 +0800

    v1

--- foundation/lockfreequeue/lock_free_queue.cpp
+++ foundation/lockfreequeue/lock_free_queue.cpp
@@ -1,14 +1,11 @@
-#include "affinity.h"
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 #include <thread>
 #include "mt_queue.h"
-#include "spsc_ring_queue.h"
 #include "show_time.h"
 
 
 mt_queue<int> q(1024);
-spsc_ring_queue<int, 1024> ring;
 
 
 const int N = 1024 * 1024;
@@ -24,28 +21,7 @@ void consumer()
 {
     for (int i = 0; i < N; ++i) {
         int value = q.pop();
-        if (value != i) {
-            fmt::println("Data Error: {} != {}", value, i);
-        }
-    }
-}
-
-void ring_producer()
-{
-    auto w = ring.writer();
-    for (int i = 0; i < N; ++i) {
-        int value = i;
-        while (w.write_n(&value, 1) != 1);
-    }
-}
-
-void ring_consumer()
-{
-    auto r = ring.reader();
-    for (int i = 0; i < N; ++i) {
-        int value = -1;
-        while (r.read_n(&value, 1) != 1);
-        if (value != i) {
+        if (value != i) [[unlikely]] {
             fmt::println("Data Error: {} != {}", value, i);
             exit(1);
         }
@@ -54,11 +30,9 @@ void ring_consumer()
 
 int main()
 {
-    for (int i = 0; i < 100; ++i) {
-        show_time _("queue");
-        std::jthread producer_thread(ring_producer);
-        std::jthread consumer_thread(ring_consumer);
-        producer_thread.join();
-        consumer_thread.join();
-    }
+    show_time _("queue");
+    std::jthread producer_thread(producer);
+    std::jthread consumer_thread(consumer);
+    producer_thread.join();
+    consumer_thread.join();
 }
